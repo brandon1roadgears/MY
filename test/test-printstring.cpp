8104bae268f604faa8dee8d518561f1d6d2b9850
daniel <danil_timchenko_1997@mail.ru>
2019-05-02 20:36:02 +0700

    Rwrite && re build

--- test/test-printstring.cpp
+++ test/test-printstring.cpp
@@ -1,26 +1,17 @@
 #include "Turing-test-head.h"
 string print_string(string row)
 {
-	cout << row << endl << endl;
   return row;
 }
 
-TEST_CASE("Printstring", "[Print]") {
-  REQUIRE(print_string("101010101010101") == "101010101010101");
-  REQUIRE(print_string("sdgfksdgvbksfhcbsvfsffalncfano") == "sdgfksdgvbksfhcbsvfsffalncfano");
-  REQUIRE(print_string("1265341265345634612534615243651234612") == "1265341265345634612534615243651234612");
-  REQUIRE(print_string("Hello world") == "Hello world");
-  REQUIRE(print_string("1!12##456&&()!23dqsa") == "1!12##456&&()!23dqsa");
-  REQUIRE(print_string("           ") == "           ");
-  REQUIRE(print_string("1") == "1");
-  REQUIRE(print_string("qw") == "qw");
-  REQUIRE(print_string("2d32") == "2d32");
-  REQUIRE(print_string("1qw34e5r") == "1qw34e5r");
-  REQUIRE(print_string("f56gjy8kv8dhr64f") == "f56gjy8kv8dhr64f");
-  REQUIRE(print_string("(qw qw qw qw qw)") == "(qw qw qw qw qw)");
-  REQUIRE(print_string("!adasasd(asdas)sdfgsdfgd!") == "!adasasd(asdas)sdfgsdfgd!");
-  REQUIRE(print_string("#sdgds(123123123) (!009hfgg)") == "#sdgds(123123123) (!009hfgg)");
-  REQUIRE(print_string("3651+123") == "3651+123");
-  REQUIRE(print_string("98-1024") == "98-1024");
-  cout <<"TEST PRINTSTRING IS SUCCESSFULL!!!";
+SCENARIO( "Check function with string return", "[string]" ) {
+	GIVEN( "String with some value" ) {
+		std::string str_test;
+		WHEN("Stings value is..."){
+			str_test = "101010101010101";
+			THEN("Function should return..."){
+				REQUIRE(print_string(str_test) == "101010101010101";
+			}
+		}
+	}
 }
