e48ba10cb1f19352ac16dedc71a77de1c116e23f
daniel <danil_timchenko_1997@mail.ru>
2019-05-02 20:16:17 +0700

    Yes

--- test/Turing-test-head.h
+++ test/Turing-test-head.h
@@ -2,10 +2,4 @@
 #include "catch.hpp"
 #include <bits/stdc++.h>
 using namespace std;
-struct action {
-    string state;
-    string exp_symbol;
-    string new_symb;
-    string move;
-    string next_state;
-};
+
