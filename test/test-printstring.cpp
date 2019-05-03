23701dd81f1c0c5199659b90de3712f65c60e98b
daniel <danil_timchenko_1997@mail.ru>
2019-05-03 14:22:51 +0700

    Mistake in test

--- test/test-printstring.cpp
+++ test/test-printstring.cpp
@@ -11,7 +11,7 @@ TEST_CASE("Aga" ,"[AGA]"){
   REQUIRE(print_string("165+354") == "165+354");
   REQUIRE(print_string("") == "12e(3) 3(2)q1" );
   REQUIRE(print_string("(123123)-(2)+(32323)=") == "(123123)-(2)+(32323)=");
-  REQUIRE(print_string("!@#$%^*") == "!@#$%^*";
+  REQUIRE(print_string("!@#$%^*") == "!@#$%^*");
   REQUIRE(print_string("(adsadadad)(asdadasdasda)") == "(adsadadad)(asdadasdasda)" );
   REQUIRE(print_string("hello world !") == "hello world !");
   REQUIRE(print_string("|abababbababa|") == "|abababbababa|");
