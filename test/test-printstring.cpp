af13d821aaa6350a698da4e807bc47d990460a9e
daniel <danil_timchenko_1997@mail.ru>
2019-05-03 14:26:15 +0700

    Dopisal test in printstr

--- test/test-printstring.cpp
+++ test/test-printstring.cpp
@@ -9,7 +9,7 @@ TEST_CASE("Aga" ,"[AGA]"){
   REQUIRE(print_string("fusdfbsdgbsdgvisdgbvd") == "fusdfbsdgbsdgvisdgbvd");
   REQUIRE(print_string("           ") == "           ");
   REQUIRE(print_string("165+354") == "165+354");
-  REQUIRE(print_string("") == "12e(3) 3(2)q1" );
+  REQUIRE(print_string("12e(3) 3(2)q1") == "12e(3) 3(2)q1" );
   REQUIRE(print_string("(123123)-(2)+(32323)=") == "(123123)-(2)+(32323)=");
   REQUIRE(print_string("!@#$%^*") == "!@#$%^*");
   REQUIRE(print_string("(adsadadad)(asdadasdasda)") == "(adsadadad)(asdadasdasda)" );
