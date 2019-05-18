7f77fdfaa92eab9af13a64048c66617ef412c0e3
daniel <danil_timchenko_1997@mail.ru>
2019-05-18 23:30:16 +0700

    True test

--- test/test-check_error.cpp
+++ test/test-check_error.cpp
@@ -37,25 +37,23 @@ int check_error(vector <action> &rules)
 	return 1;
 }
 TEST_CASE("Check error test:", "[Checkerrortest]"){
-	std::vector <action> rules(1);
+	std::vector <action> rules;
+	rules.resize(1);
 	rules[0].state = "00"; rules[0].exp_symbol = "1"; rules[0].new_symb = "0"; rules[0].move = "r"; rules[0].next_state = "01";
 	REQUIRE(check_error(rules) == 1);
 
-	std::vector <action> rules(1);
 	rules[0].state = "00"; rules[0].exp_symbol = "W"; rules[0].new_symb = "t"; rules[0].move = "l"; rules[0].next_state = "qwerw";
 	REQUIRE(check_error(rules) == 1);
 
-	std::vector <action> rules(1);
 	rules[0].state = "00"; rules[0].exp_symbol = "!"; rules[0].new_symb = "^"; rules[0].move = "n"; rules[0].next_state = "halt";
 	REQUIRE(check_error(rules) == 1);
 
-	std::vector <action> rules(3);
+	rules.resize(3);
 	rules[0].state = "00"; rules[0].exp_symbol = "1"; rules[0].new_symb = "0"; rules[0].move = "r"; rules[0].next_state = "next";
 	rules[1].state = "next"; rules[1].exp_symbol = "0"; rules[1].new_symb = "1"; rules[1].move = "r"; rules[1].next_state = "prehalt";
 	rules[2].state = "prehalt"; rules[2].exp_symbol = "_"; rules[2].new_symb = "!"; rules[2].move = "n"; rules[2].next_state = "halt";
 	REQUIRE(check_error(rules) == 1);
 
-	std::vector <action> rules(3);
 	rules[0].state = "00"; rules[0].exp_symbol = "1"; rules[0].new_symb = "0"; rules[0].move = "r"; rules[0].next_state = "next";
 	rules[1].state = "next"; rules[1].exp_symbol = "0"; rules[1].new_symb = "1"; rules[1].move = "r"; rules[1].next_state = "prehalt";
 	rules[2].state = "prehalt"; rules[2].exp_symbol = "_"; rules[2].new_symb = "!"; rules[2].move = "n"; rules[2].next_state = "halt";
